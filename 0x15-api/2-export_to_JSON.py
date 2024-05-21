5c302cec33d09d9693dcf5b7c43e3c1609740b95
dahunsi-dami <gbemigadare@gmail.com>
2024-05-21 05:28:29 +0100

    Export task 0 in given format as json.

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Returns info about employee's todo list progress,
+and export data in json format.
+"""
+
+import requests
+import sys
+import json
+
+
+if __name__ == '__main__':
+    empId = sys.argv[1]
+    userUrl = "https://jsonplaceholder.typicode.com/users/"
+    userInfo = userUrl + empId
+    taskInfo = userUrl + empId + "/todos"
+
+    # Send API requests
+    userResponse = requests.get(userInfo)
+    taskResponse = requests.get(taskInfo)
+
+    # Get employee name from userResponse
+    empUName = userResponse.json().get('username')
+
+    # Get tasks from taskResponse as todos
+    todos = taskResponse.json()
+
+    fname = f'{sys.argv[1]}.json'
+
+    valoos = {empId: []}
+
+    for item in todos:
+        valoos[empId].append({
+            "task": item['title'],
+            "completed": item['completed'],
+            "username": empUName
+        })
+
+    with open(fname, "w") as file:
+        json.dump(valoos, file)
